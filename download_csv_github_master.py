d5457402edaaca81543f8167e8850d4d59735810
Saransh-Tiwari-2002 <84573388+Saransh-Tiwari-2002@users.noreply.github.com>
2022-04-14 13:41:05 +0530

    Update download_csv_github_master.py

--- download_csv_github_master.py
+++ download_csv_github_master.py
@@ -5,7 +5,7 @@ import os
 def get_json_from_github():
     try: os.remove(f'player_file.csv')
     except: pass
-    g = Github('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')
+    g = Github('ENTER GITHUB TOKEN HERE')
     repository=g.get_user().get_repo('efootball_player_files')
     for x in ['1a', '1b', '1c', '1d','2a', '2b', '2c', '2d','3a', '3b', '3c', '3d','4a', '4b', '4c', '4d','5a', '5b', '5c', '5d','6a', '6b', '6c', '6d','7a', '7b', '7c','7d','8a', '8b']:
         data_csv = pandas.read_csv((repository.get_contents(f'{x}.csv').download_url))    
